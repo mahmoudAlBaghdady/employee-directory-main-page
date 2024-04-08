fdabec3bc423d36d0aaeb8c7ddc09226a68ba06a
mahmoudAlBaghdady <mahmoudalbaghdadyy@gmail.com>
2024-04-08 05:01:35 +0300

    Fix footer

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
             <section className=" pt-5">
                 <div className="container text-center text-md-start mt-5">
                     <div className="row mt-3">
-                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <div className="col-md-6 col-lg-4 col-xl-3 mx-auto mb-4">
                             <h6 className="text-uppercase fw-bold mb-4"><img src="/img/logo2.webp" alt="Logo" width="50" height="44" className="d-inline-block me-1 mb-1"/>Employee Directory</h6>
                             <p>For assistance or inquiries about our directory management platform, please contact us at support@employeedirect.com or visit our support page.</p>
                         </div>
 
-                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <div className="col-md-6 col-lg-2 col-xl-2 mx-auto mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">Features</h6>
                             <p><a href="#!" className="text-reset text-decoration-none">Employee Profiles</a></p>
                             <p><a href="#!" className="text-reset text-decoration-none">Search & Filter</a></p>
@@ -20,7 +20,7 @@ const Footer = () => {
                             <p><a href="#!" className="text-reset text-decoration-none">Contact Management</a></p>
                         </div>
                 
-                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <div className="col-md-6 col-lg-2 col-xl-2 mx-auto mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">Resources</h6>
                             <p><a href="#!" className="text-reset text-decoration-none">FAQ</a></p>
                             <p><a href="#!" className="text-reset text-decoration-none">Support Center</a></p>
@@ -28,10 +28,12 @@ const Footer = () => {
                             <p><a href="#!" className="text-reset text-decoration-none">Privacy Policy</a></p>
                         </div>
                 
-                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <div className="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">Contact Us</h6>
-                            <p><i className="fas fa-phone me-3"></i> + 961 78 87 16 49</p>
-                            <p><i className="fas fa-envelope me-3"></i> support@employeedirect.com</p>
+                            <p>+ 961 78 87 16 49</p>
+                            <p>support@employeedirectory.site</p>
+                            <p>www.employeedirectory.site</p>
+                            <p>dashboard.employeedirectory.site</p>
                         </div>
                     </div>
                     <div className="col-12 pt-5 pb-2 text-center">
